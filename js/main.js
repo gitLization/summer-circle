09ce7f78d6b76a21d78f640fde1761211f970976
dahyeko <dalgona92@sungshin.ac.kr>
2021-03-24 23:01:02 +0900

    Complete header and nav menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
+// song__info라는 id를 가진 문서 요소 선택
 const songEl = document.getElementById("song__info");
+// headset--on이라는 id를 가진 문서 요소 선택
 const musicOnEl = document.getElementById('headset--on');
+// headset--off이라는 id를 가진 문서 요소 선택
 const musicOffEl = document.getElementById('headset--off');
 
+// 오디오 생성
 const audio = new Audio();
+// 플레이리스트 배열
 const playlists = [
     { 
         src: "./../audio/long_dream.mp3", 
@@ -11,27 +16,38 @@ const playlists = [
     }
 ];
 
+// 윈도우가 로드할 시 작동하는 익명의 함수
 window.addEventListener('load', function() {
     playlists.map((el) => {
+        // playlists 배열에 있는 요소의 src에 해당하는 값을 오디오 소스로 설정
         audio.src = el.src
+        // playlists 배열에 있는 요소의 artist에 해당하는 값과
+        // song에 해당하는 값을 문서에 대입
         songEl.innerHTML = `${el.artist} - ${el.song}`
     })
+    // 오디오 재생
     audio.play()
+    // 오디오 반복 재생 true
     audio.loop = 'true'
 })
 
-
+// musicOnEl에 클릭 이벤트 발생할 시 작동하는 익명의 함수
 musicOnEl.addEventListener('click', function() {
+    // 헤드셋 on 아이콘을 보이지 않게 함
     musicOnEl.style.display = 'none'
+    // 헤드셋 off 아이콘이 나타남
     musicOffEl.style.display = 'flex'
-
+    // 오디오 일시정지
     audio.pause();
 })
 
+// musicOffEl에 클릭 이벤트 발생할 시 작동하는 익명의 함수
 musicOffEl.addEventListener('click', function() {
+    // 헤드셋 off 아이콘을 보이지 않게 함
     musicOnEl.style.display = 'flex'
+    // 헤드셋 on 아이콘이 나타남
     musicOffEl.style.display = 'none'
-
+    // 오디오 재생
     audio.play();
 })
 
